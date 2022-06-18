d1306a5204f264e2c0608de99b028575df723369
HotPotatoC <juandotulung@gmail.com>
2022-06-18 21:12:27 +0700

    feat(database): updated `UserSeeder` and `UserFactory`

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -19,9 +19,12 @@ class UserFactory extends Factory
     {
         return [
             'name' => $this->faker->name(),
+            'username' => $this->faker->userName(),
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => bcrypt('password'), // password
+            'type' => $this->faker->randomElement(['project', 'collective', 'event', 'organization', 'personal']),
+            'avatar_url' => $this->faker->imageUrl(),
             'remember_token' => Str::random(10),
         ];
     }
