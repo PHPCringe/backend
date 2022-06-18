96d43e24e4d1db08bee2673576e6b7f6be03be8b
HotPotatoC <juandotulung@gmail.com>
2022-06-18 22:39:34 +0700

    refactor(transaction): change property access method to object

--- app/Http/Controllers/TransactionDonationController.php
+++ app/Http/Controllers/TransactionDonationController.php
@@ -11,14 +11,14 @@ class TransactionDonationController extends Controller
     public function store(Request $request)
     {
         $createdTransaction = Transaction::create([
-            'from_user_id' => $request['from_user_id'],
-            'to_user_id' => $request['to_user_id'],
-            'type' => $request['type'],
-            'issued_by' => 0,
-            'currency_id' => $request['currency_id'],
-            'title' => $request['title'],
-            'description' => $request['description'],
-            'amount' => $request['amount'],
+            'from_user_id' => $request->from_user_id,
+            'to_user_id' => $request->to_user_id,
+            'type' => $request->type,
+            'issued_by' => 0, // Since this is a donation, issued_by is always 0
+            'currency_id' => $request->currency_id,
+            'title' => $request->title,
+            'description' => $request->description,
+            'amount' => $request->amount,
         ]);
 
         return $this->responseJson(201, 'Successfully saved donation transaction', $createdTransaction);
--- app/Http/Controllers/TransactionExpenseController.php
+++ app/Http/Controllers/TransactionExpenseController.php
@@ -10,14 +10,14 @@ class TransactionExpenseController extends Controller
     public function store(Request $request)
     {
         $createdTransaction = Transaction::create([
-            'from_user_id' => $request['from_user_id'],
-            'to_user_id' => $request['to_user_id'],
-            'type' => $request['type'],
-            'issued_by' => $request['issued_by'],
-            'currency_id' => $request['currency_id'],
-            'title' => $request['title'],
-            'description' => $request['description'],
-            'amount' => $request['amount'],
+            'from_user_id' => $request->from_user_id,
+            'to_user_id' => $request->to_user_id,
+            'type' => $request->type,
+            'issued_by' => $request->issued_by,
+            'currency_id' => $request->currency_id,
+            'title' => $request->title,
+            'description' => $request->description,
+            'amount' => $request->amount,
         ]);
 
         return $this->responseJson(201, 'Successfully saved expense transction', $createdTransaction);
